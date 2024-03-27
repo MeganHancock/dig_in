e8af4761976cd0a5df96a09f502f5f4d3a491b82
MeganHancock <mgnhnck@gmail.com>
2024-03-27 14:20:14 -0600

    recipes table

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,3 +1,4 @@
+-- Active: 1711142108469@@35.87.147.206@3306@honest_wendigo_9013b4_db
 CREATE TABLE IF NOT EXISTS accounts(
   id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
@@ -6,3 +7,15 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipes(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+  title VARCHAR(100) NOT NULL,
+  instructions VARCHAR(2000) NOT NULL,
+  category ENUM("Soup", "Pasta", "Fish", "Salad", "Beef", "Burgers", "Specialty Coffee", "Italian", "Cheese", "Mexican", "Other") DEFAULT "Other",
+  img VARCHAR(1000) NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY(creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+  )
