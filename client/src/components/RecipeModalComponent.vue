99906514751534d1667e6d9d6383d0a9d534a1bf
MeganHancock <mgnhnck@gmail.com>
2024-04-01 14:08:40 -0600

    creating and drawing new ingredients

--- client/src/components/RecipeModalComponent.vue
+++ client/src/components/RecipeModalComponent.vue
@@ -21,6 +21,7 @@
                 </p>
               </div>
 
+              <!-- NOTE INGREDIENTS CARD AND FORM -->
               <div class="card m-1 p-1">
                 <h5>Ingredients</h5>
                 <ul v-for="ingredient in activeRecipeIngredients" :key="ingredient.id">
@@ -28,18 +29,19 @@
                     {{ ingredient.name }} - {{ ingredient.quantity }}
                   </li>
                 </ul>
-                <form @submit="createIngredient(activeRecipe.id)">
-                  <div class="input-group m-1">
-                    <input v-model="editableIngredientData.ingredient" type="text" class="form-control"
-                      placeholder="Add Ingredient" aria-label="addIngredient" aria-describedby="button-addon2" required>
-                  </div>
-                  <div class="input-group m-1">
-                    <input v-model="editableIngredientData.quantity" type="text" class="form-control"
-                      placeholder="Add Quantity" aria-label="Recipient's username" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2" required>+</button>
-                  </div>
-
-                </form>
+                <div v-if="activeRecipe.creatorId = account.id">
+                  <form @submit="createIngredient(activeRecipe.id)" class="d-flex">
+                    <div class="input-group m-1">
+                      <input v-model="editableIngredientData.name" type="text" class="form-control w-"
+                        placeholder="Name" aria-label="addIngredient" aria-describedby="button-addon2" required>
+                    </div>
+                    <div class="input-group m-1">
+                      <input v-model="editableIngredientData.quantity" type="text" class="form-control"
+                        placeholder="Quantity" aria-label="Recipient's username" aria-describedby="button-addon2">
+                      <button class="btn btn-outline-secondary" type="submit" id="button-addon2" required>+</button>
+                    </div>
+                  </form>
+                </div>
 
               </div>
             </div>
@@ -47,12 +49,12 @@
 
         </div>
 
-
+        <!-- NOTE FOOTER BUTTONS -->
         <div class="modal-footer d-flex justify-content-between">
           <div v-if="account.id = activeRecipe.creatorId">
             <button @click="removeRecipe(activeRecipe.id)" class="btn btn-warning" type="button">Delete Recipe</button>
           </div>
-          <div>
+          <div class="d-flex">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             <button type="button" class="btn btn-primary" title="Favorite this recipe"><i
                 class="mdi mdi-heart"></i></button>
@@ -76,7 +78,7 @@ import { Modal } from 'bootstrap';
 
 export default {
   setup() {
-    const editableIngredientData = ref({ ingredient: '', quantity: '' })
+    const editableIngredientData = ref({ name: '', quantity: '' })
     return {
       activeRecipe: computed(() => AppState.activeRecipe),
       activeRecipeIngredients: computed(() => AppState.activeRecipeIngredients),
@@ -104,7 +106,7 @@ export default {
       async createIngredient(recipeId) {
         try {
           const newIngredient = await ingredientsService.createIngredient(editableIngredientData.value, recipeId)
-          editableIngredientData.value = { ingredient: '', quantity: '' }
+          editableIngredientData.value = { name: '', quantity: '' }
         }
         catch (error) {
           Pop.error(error);
--- client/src/services/IngredientsService.js
+++ client/src/services/IngredientsService.js
@@ -4,11 +4,16 @@ import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
 class IngredientsService{
-createIngredient(ingredientData, recipeId) {
-throw new Error('Method not implemented.')
+async createIngredient(ingredientData, recipeId) {
+ingredientData.recipeId = recipeId
+// logger.log('ingredient data', ingredientData)
+const response = await api.post('api/ingredients', ingredientData)
+// logger.log('new ingredient frm api', response.data)
+const newIngredient = new Ingredient(response.data)
+AppState.activeRecipeIngredients.push(newIngredient)
 }
 async getRecipeIngredientsByRecipeId(recipeId) {
-    AppState.activeRecipeIngredients = null
+AppState.activeRecipeIngredients = null
 const response = await api.get(`api/recipes/${recipeId}/ingredients`)
 AppState.activeRecipeIngredients = response.data.map(ingredientPOJO => new Ingredient(ingredientPOJO))
 }
